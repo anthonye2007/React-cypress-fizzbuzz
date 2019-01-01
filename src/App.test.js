99a5d4694ae4084fb7f7e4bf447cd9a57f693582
Anthony Elliott <aelliott@pillartechnology.com>
2019-01-01 15:56:49 -0600

    WIP: attempting to stub fetch

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import App from './App';
 import { mount } from 'enzyme';
 
+
 describe('Fizzbuzz', () => {
   let wrapper;
   beforeEach(() => {
@@ -14,6 +15,22 @@ describe('Fizzbuzz', () => {
 
   it('should not show result before clicking button', () => {
     expect(getResult(wrapper)).toEqual('');
+  });
+
+  it('should display Fizz when number is 3', () => {
+    // fake out network request
+    global.fetch = jest.fn(() => Promise.resolve( { 
+      json: () => {
+        return { value: 3}
+      }
+    }) );
+
+    // click button
+    wrapper.find('button#submit').simulate('click');
+    wrapper.update();
+
+    // assert result is Fizz
+    expect(getResult(wrapper)).toEqual("Fizz");
   });
 });
 
